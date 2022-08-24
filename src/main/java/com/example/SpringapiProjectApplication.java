92c21adb0743decdebd19eb09756568b9e4750c5
shefali <shefalisandlya4321@gmail.com>
2022-08-24 09:49:34 +0530

    First Commit

---
+++ src/main/java/com/example/SpringapiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringapiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringapiProjectApplication.class, args);
+	}
+
+}
